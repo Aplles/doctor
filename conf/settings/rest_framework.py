de9c4d621ca1284f12e9640ff3e918a97ec08fac
SomeBody <maxim.karaichev@saltpepper.ru>
2024-02-24 00:32:42 +0300

    fix page size

--- conf/settings/rest_framework.py
+++ conf/settings/rest_framework.py
@@ -7,5 +7,5 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
-    "PAGE_SIZE": 3,
+    "PAGE_SIZE": 8,
 }
