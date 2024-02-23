2d9c2601a7fcdab0b3fc47d0b37d6a3b57af0505
SomeBody <maxim.karaichev@saltpepper.ru>
2024-02-23 22:01:37 +0300

    fix cors

--- conf/settings/corsheaders.py
+++ conf/settings/corsheaders.py
@@ -1,6 +1,8 @@
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = ['http://localhost:3000/']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000/']
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1', 'http://localhost', 'http://localhost:8000', 'http://localhost:5000',
+                        'http://localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'http://localhost', 'http://localhost:8000', 'http://localhost:5000',
+                        'http://localhost:3000']
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
