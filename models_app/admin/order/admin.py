c99196a07ac4b7f0be24f4791a20d3f9a16bfa2f
SomeBody <maxim.karaichev@saltpepper.ru>
2024-03-05 18:23:37 +0300

    add total price

--- models_app/admin/order/admin.py
+++ models_app/admin/order/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Sum, F
 
 from models_app.admin.product_amount.admin import ProductAdminInline
 from models_app.models import Order
@@ -20,3 +21,28 @@ class OrderAdmin(admin.ModelAdmin):
         'phone',
     )
     inlines = (ProductAdminInline, )
+    readonly_fields = (
+        'id',
+        'created_at',
+        'updated_at',
+        'total_price',
+    )
+    fields = (
+        'id',
+        'first_name',
+        'phone',
+        'address',
+        'description',
+        'direction_image',
+        'delivery',
+        'created_at',
+        'updated_at',
+        'total_price',
+    )
+
+    def total_price(self, obj):
+        return obj.products.annotate(
+            product_total_price=F('price') * F('product_amounts__amount')
+        ).aggregate(total=Sum('product_total_price'))['total']
+
+    total_price.short_description = 'Общая стоимость услуг'
--- models_app/admin/product_amount/admin.py
+++ models_app/admin/product_amount/admin.py
@@ -6,6 +6,12 @@ from models_app.models import ProductAmount
 class ProductAdminInline(admin.TabularInline):
     model = ProductAmount
     extra = 0
+    readonly_fields = ['product_price']
+
+    def product_price(self, obj):
+        return obj.product.price
+
+    product_price.short_description = 'Цена'
 
 
 @admin.register(ProductAmount)
