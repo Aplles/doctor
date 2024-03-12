3776df63e03ce11f5c128d8497a93bbe0b80b52c
SomeBody <maxim.karaichev@saltpepper.ru>
2024-03-12 21:20:40 +0300

    fix cors

--- conf/settings/corsheaders.py
+++ conf/settings/corsheaders.py
@@ -1,17 +1,4 @@
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = ['*']
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1',
-    'http://localhost',
-    'http://localhost:8000',
-    'http://localhost:5000',
-    'http://localhost:3000',
-    'http://127.0.0.1:8000',
-    'https://domdoctor.pro'
-]
-CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1',
     'http://localhost',
     'http://localhost:8000',
